af86382d91598edb5fe83a2835ed73e101cda054
ZKala <46380288+ZKala@users.noreply.github.com>
2022-08-14 13:19:34 +0800

    Update notes.js

--- projects/notes.js
+++ projects/notes.js
@@ -16,8 +16,8 @@ function addNewNote(text = '') {
 
  note.innerHTML = `
  <div class="tools">
-   <button class="edit"><i class="fas fa-edit"></i></button>
-   <button class="delete"><i class="fas fa-trash-alt"></i></button>
+   <button class="edit"><i class="fas fa-edit"></i>Edit</button>
+   <button class="delete"><i class="fas fa-trash-alt"></i>Del</button>
  </div>
 
  <div class="main ${text ? "" : "hidden"}"></div>
